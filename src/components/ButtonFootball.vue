0861eb365c9dbeb9553c83a738de4202ec526041
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2024-11-22 17:56:24 +1100

    Talking to backend

--- src/components/ButtonFootball.vue
+++ src/components/ButtonFootball.vue
@@ -1,33 +1,38 @@
 <script>
 import { ref, onMounted } from 'vue';
-import { TabulatorFull as Tabulator } from 'tabulator-tables';
-// import 'tabulator-tables/dist/css/tabulator.min.css';
-import 'tabulator-tables/dist/css/tabulator_bootstrap5.min.css';
+import { TabulatorFull as Tabulator } from 'tabulator-tables'
+import 'tabulator-tables/dist/css/tabulator_bootstrap5.min.css'
+import axios from 'axios'
 
 export default {
   setup() {
     const table = ref(null);
+    const items = ref([]);
 
-    onMounted(() => {
+    onMounted(async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/teams?name=Corinthians')
+        items.value = await response.data
+      } catch (error) {
+        console.error('Error fetching API data:', error)
+      }
+      
       new Tabulator(table.value, {
-        data: [
-          { name: "Oli Bob", age: "12" },
-          { name: "Mary May", age: "1" },
-          { name: "Christine Lobowski", age: "42" },
-          { name: "Brendon Philips", age: "125" },
-          { name: "Margret Marmajuke", age: "16" },
-        ], // Your data here
+        data: [ items.value ],
         layout: "fitColumns",
         columns: [
           { title: "Name", field: "name" },
-          { title: "Age", field: "age" },
+          { title: "Type", field: "type" },
+          { title: "Full Name", field: "fullName" },
+          { title: "Foundation", field: "foundation" },
+          { title: "City", field: "city" },
+          { title: "Country", field: "country" },
+          { title: "Logo", field: "logoImgFile" },
         ],
       });
     });
 
-    return {
-      table,
-    };
+    return { table };
   },
 };
 </script>
