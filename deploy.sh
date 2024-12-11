200831eb606b10fb28df87e2f6f409fd05db4f28
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2024-12-11 15:19:13 +1100

    Deployment to Firebase CI/CD (cont...)

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo ">>> Step 1: Clean the repo"
+echo ">>> Step 1: Instal all npm dependencies"
+npm install
+
+echo ">>> Step 2: Clean the repo"
 sbt clean
 
-echo ">>> Step 2: Perform full JS link (compile and build Scala.js code)"
+echo ">>> Step 3: Perform full JS link (compile and build Scala.js code)"
 sbt fullLinkJS
 
-echo ">>> Step 3: Prepare public assets directory for hosting in Firebase"
+echo ">>> Step 4: Prepare public assets directory for hosting in Firebase"
 ./prep_public.sh public
 
-echo ">>> Step 4: Instal firebase-tools (it's a node tool)"
+echo ">>> Step 5: Instal firebase-tools (it's a node tool)"
 npm install -g firebase-tools
-
-echo ">>> Step 5: Instal all npm dependencies"
-npm install
 
 echo ">>> Step 6: Build the website with Vite (vite build)"
 npm run build
--- prep_public.sh
+++ prep_public.sh
@@ -4,5 +4,11 @@
 public_dir=$1
 scala_ver=3.5.2
 
+rm -rf $public_dir/scss
+
+mkdir -p $public_dir/scss
+
+#cp -R target/scala-$scala_ver/classes/css/* $public_dir/css
+
 cp firebase.json $public_dir
 cp index.html $public_dir
