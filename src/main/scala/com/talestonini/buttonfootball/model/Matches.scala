758a7bc77e60d588267fbeb102ad376b033419f8
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2025-04-17 21:35:47 +1000

    Fix in nullability of score fields in Matches

--- src/main/scala/com/talestonini/buttonfootball/model/Matches.scala
+++ src/main/scala/com/talestonini/buttonfootball/model/Matches.scala
@@ -9,9 +9,9 @@ import org.http4s.circe.CirceEntityCodec._
 object Matches:
 
   case class Match(id: Id, championship: String, numEdition: Int, `type`: String, teamA: String, teamB: String,
-                   teamALogoImgFile: String, teamBLogoImgFile: String, numGoalsTeamA: Int, numGoalsTeamB: Int,
-                   numGoalsExtraA: Option[Int], numGoalsExtraB: Option[Int], numGoalsPntA: Option[Int],
-                   numGoalsPntB: Option[Int]) extends Model {
+                   teamALogoImgFile: String, teamBLogoImgFile: String, numGoalsTeamA: Option[Int],
+                   numGoalsTeamB: Option[Int], numGoalsExtraA: Option[Int], numGoalsExtraB: Option[Int],
+                   numGoalsPntA: Option[Int], numGoalsPntB: Option[Int]) extends Model {
 
     def winner(): Option[String] =
       if (s"${numGoalsPntA.getOrElse(0)}${numGoalsExtraA.getOrElse(0)}$numGoalsTeamA".toInt >
