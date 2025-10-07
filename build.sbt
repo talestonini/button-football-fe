4a72b419111250176707a9d92d0af8bca07ada52
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2025-10-07 23:12:20 +1100

    Minor ranking tab layout improvement

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ lazy val buttonFootballFrontEnd = project.in(file("."))
   .enablePlugins(ScalablyTypedConverterExternalNpmPlugin)
   .enablePlugins(BuildInfoPlugin)
   .settings(
-    version := "0.4.2",
+    version := "0.4.3",
     scalaVersion := scalaVer,
 
     // Tell Scala.js that this is an application with a main method
--- src/main/scala/com/talestonini/buttonfootball/component/RankingTabContent.scala
+++ src/main/scala/com/talestonini/buttonfootball/component/RankingTabContent.scala
@@ -13,6 +13,35 @@ object RankingTabContent:
 
   def apply(): Element =
     div(
+      cls := "container border bg-white p-3 text-end",
+      buildStyleAttr("overflow-x: auto"),
+      child <-- vRankings.signal.map(rs => {
+        val vRankings: Var[List[Ranking]] = Var(rs)
+        val ws = Window.size()
+        val smallish = ws == Size.Small || ws == Size.Medium
+        Table[Ranking](vRankings, List(
+          Column(if (smallish) RankingPositionShortToken else RankingPositionToken, 9),
+          TeamColumn(3),
+          Column(EmptyToken, 2, "text-start", !smallish,
+            Some((teamName: String) => span(text <-- I18n(teamName, TeamTranslationMap)))
+          ),
+          Column(if (smallish) RankingPointsShortToken else RankingPointsToken, 8),
+          Column(if (smallish) RankingChampionshipsShortToken else RankingChampionshipsToken, 18),
+          Column(if (smallish) RankingParticipationsShortToken else RankingParticipationsToken, 7),
+          Column(if (smallish) RankingAveragePositionShortToken else RankingAveragePositionToken, 6),
+          // Decided not to have these fields in the table:
+          // Column(if (smallish) RankingBestPositionShortToken else RankingBestPositionToken, 4),
+          // Column(if (smallish) RankingWorstPositionShortToken else RankingWorstPositionToken, 5),
+          Column(if (smallish) StandingsPointsShortToken else StandingsPointsToken, 10),
+          Column(if (smallish) StandingsMatchesShortToken else StandingsMatchesToken, 11),
+          Column(if (smallish) StandingsWinsShortToken else StandingsWinsToken, 12),
+          Column(if (smallish) StandingsDrawsShortToken else StandingsDrawsToken, 13),
+          Column(if (smallish) StandingsLossesShortToken else StandingsLossesToken, 14),
+          Column(if (smallish) StandingsGoalsScoredShortToken else StandingsGoalsScoredToken, 15),
+          Column(if (smallish) StandingsGoalsConcededShortToken else StandingsGoalsConcededToken, 16),
+          Column(if (smallish) StandingsGoalsDiffShortToken else StandingsGoalsDiffToken, 17)
+        ))
+      }),
       div(
         cls := "text-start",
         text <-- I18n(RankingUpToEditionNoteToken).combineWith(vSelectedEdition.signal)
@@ -61,36 +90,7 @@ object RankingTabContent:
             )
           )
         )
-      ),
-      cls := "container border bg-white p-3 text-end",
-      buildStyleAttr("overflow-x: auto"),
-      child <-- vRankings.signal.map(rs => {
-        val vRankings: Var[List[Ranking]] = Var(rs)
-        val ws = Window.size()
-        val smallish = ws == Size.Small || ws == Size.Medium
-        Table[Ranking](vRankings, List(
-          Column(if (smallish) RankingPositionShortToken else RankingPositionToken, 9),
-          TeamColumn(3),
-          Column(EmptyToken, 2, "text-start", !smallish,
-            Some((teamName: String) => span(text <-- I18n(teamName, TeamTranslationMap)))
-          ),
-          Column(if (smallish) RankingPointsShortToken else RankingPointsToken, 8),
-          Column(if (smallish) RankingChampionshipsShortToken else RankingChampionshipsToken, 18),
-          Column(if (smallish) RankingParticipationsShortToken else RankingParticipationsToken, 7),
-          Column(if (smallish) RankingAveragePositionShortToken else RankingAveragePositionToken, 6),
-          // Decided not to have these fields in the table:
-          // Column(if (smallish) RankingBestPositionShortToken else RankingBestPositionToken, 4),
-          // Column(if (smallish) RankingWorstPositionShortToken else RankingWorstPositionToken, 5),
-          Column(if (smallish) StandingsPointsShortToken else StandingsPointsToken, 10),
-          Column(if (smallish) StandingsMatchesShortToken else StandingsMatchesToken, 11),
-          Column(if (smallish) StandingsWinsShortToken else StandingsWinsToken, 12),
-          Column(if (smallish) StandingsDrawsShortToken else StandingsDrawsToken, 13),
-          Column(if (smallish) StandingsLossesShortToken else StandingsLossesToken, 14),
-          Column(if (smallish) StandingsGoalsScoredShortToken else StandingsGoalsScoredToken, 15),
-          Column(if (smallish) StandingsGoalsConcededShortToken else StandingsGoalsConcededToken, 16),
-          Column(if (smallish) StandingsGoalsDiffShortToken else StandingsGoalsDiffToken, 17)
-        ))
-      })
+      )
     )
 
 end RankingTabContent
